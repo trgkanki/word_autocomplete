27b9eab0156a66b6cc237bce93ff52f338c24a79
Hyun Woo Park <phu54321@naver.com>
2020-06-02 13:02:27 +0900

    :bug: ignore image occlusion enhanced type's id from candidate

--- src/wordSet.py
+++ src/wordSet.py
@@ -1,9 +1,9 @@
 import re
 from .stripHTML import stripHTML
+from anki.utils import splitFields, joinFields
 
 wordSetCache = {}
 alphaNumeric = re.compile("[a-zA-Z][a-zA-Z0-9]{4,}")
-
 
 def createWordSet(col):
     """ Initialize wordSet from preexisting collections """
@@ -11,11 +11,25 @@ def createWordSet(col):
 
     wordSet = set()
 
-    for (field,) in col.db.execute("select flds from notes"):
+    # Hack for image occlusion enhanced type model
+    # ID field (first field) of io_occ addon note type should be
+    # excluded from the search
+    imgoccModel = col.models.byName('Image Occlusion Enhanced')
+    if imgoccModel:
+        iocc_mid = imgoccModel['id']
+    else:
+        iocc_mid = None
+
+
+    for (field, mid) in col.db.execute("select flds, mid from notes"):
         try:
             wordSet.update(wordSetCache[field])
         except KeyError:
             rawField = field
+
+            # Ignore first field of image occlusion enhanced note
+            if mid == iocc_mid:
+                field = joinFields(splitFields(field)[1:])
 
             field = re.sub(r'\[sound:.*?\]', '', field)
             field = re.sub(r'<.*?>', '', field)
